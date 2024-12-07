3a5dbd0e66f1cc687126badcdda37016dad626cc
nicolle <nicolleborges3645@outlook.com>
2024-12-07 18:14:32 -0300

    Versão final.

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -7,6 +7,12 @@ import java.io.IOException;
 public class Principal {
     public static void main(String[] args) throws IOException {
         int ano = Integer.parseInt(JOptionPane.showInputDialog("Insira um ano: "));
+        if(ano < 1960 || ano > 2023){
+            while(ano < 1960 || ano > 2023) {
+                JOptionPane.showMessageDialog(null, "Não há dados para o ano inserido. Digite novamente.");
+                ano = Integer.parseInt(JOptionPane.showInputDialog("Insira um ano: "));
+            }
+        }
         PopController pop = new PopController();
         {
             try {
